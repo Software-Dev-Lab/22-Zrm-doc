acee120d65e6e9c4c0e8d2cfcdb76ec14aa3f9c7
ZRMYDYCG <547471919@qq.com>
2024-10-23 14:46:10 +0800

    update

--- 算法与数据结构/队列/循环队列.js
+++ 算法与数据结构/队列/循环队列.js
@@ -1,14 +1,21 @@
-const { get } = require("http");
-
 /**
  * @description 循环队列: 因为单链队列在出队操作的时候需要 O(n) 的时间复杂度，所以引入了循环队列。循环队列的出队操作平均是 O(1) 的时间复杂度。
  * */
 class SqQueue {
-    constructor() {}
+    constructor(length) {
+        this.queue = new Array(length + 1)
+        // 队头
+        this.first = 0
+        // 队尾
+        this.last = 0
+        // 队列长度
+        this.size = 0
+    }
     /**
      * 入队
     */
-   enQueue() {}
+   enQueue() {
+   }
     /**
      * 出队
      * */
@@ -22,7 +29,9 @@ class SqQueue {
     /**
      * 队列长度
      * */ 
-    getLength() {}
+    getLength() {
+        return this.queue.length - 1
+    }
     /**
      * 判断队列是否为空
      * */ 
@@ -32,3 +41,6 @@ class SqQueue {
      * */
     resize() {}
 }
+
+const test = new SqQueue(5)
+console.log(test.getLength())
